5582347eec3bab57133bf6b60ee0c3ed75d18887
wellingtonmr <wellingtonmourar@gmail.com>
2024-09-09 10:46:36 -0300

    Results redisign
    Added a column (monthly interest) to the table and final sums divs

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -20,31 +20,48 @@ function results() {
 
 	let finValue = iniInvest;
 	let invested = iniInvest;
+	let monInterest = 0.0;
 	let interest = 0.0;
 
 	document.getElementById("result").innerHTML =
-		`<table id="result-table">
+		`<div class="totals" id="total-invested">
+			Invested:<br />
+		</div>
+		<div class="totals" id="total-interest">
+			Interest:<br />
+		</div>
+		<div class="totals red" id="total-sum">
+			Final Sum:<br />
+		</div>
+		<table id="result-table">
 			<tr>
 				<th>Month</th>
 				<th>Invested</th>
 				<th>Interest</th>
+				<th>Total Int.</th>
 				<th>Total</th>
 			</tr>
 		</table>`;
 
 	for(let i = 0; i < time; i++) {
-		finValue += finValue * (rate/100);
-		interest += finValue * (rate/100);
+		monInterest = finValue * (rate/100);
+		finValue += monInterest;
+		interest += monInterest;
 		finValue += monInvest;
 		invested += monInvest;
 		document.getElementById("result-table").innerHTML +=
 			`<tr ${i%2 == 0 ? 'class="white"' : 'class="gray"' }>
-				<td>${i+1}</td>
-				<td><span>R$</span>${invested.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}</td>
-				<td><span>R$</span>${interest.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}</td>
-				<td><span>R$</span>${finValue.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}</td>
+				<td class="month">${i+1}</td>
+				<td><span>U$</span>${invested.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}</td>
+				<td><span>U$</span>${monInterest.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}</td>
+				<td><span>U$</span>${interest.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}</td>
+				<td><span>U$</span>${finValue.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}</td>
 			</tr>`
 	}
-
-	console.log(finValue);
+	document.getElementById("total-invested").innerHTML +=
+		`U$ ${invested.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+	document.getElementById("total-interest").innerText +=
+		`U$ ${interest.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+	document.getElementById("total-sum").innerText +=
+		`U$ ${finValue.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
 }
